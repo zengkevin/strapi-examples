3b51a01ee202d97d8f295f739221f38585258c61
soupette <cyrillopez@MacBook-Pro-de-Cyril.local>
2018-06-11 13:03:12 +0200

    SDK with providers

--- create-react-app-sdk/src/components/SocialLink/index.js
+++ create-react-app-sdk/src/components/SocialLink/index.js
@@ -9,12 +9,13 @@ import { capitalize } from 'lodash';
 import PropTypes from 'prop-types';
 
 import Button from '../../components/Button';
+import strapi from '../../utils/Strapi';
 
 import './styles.css';
 
 function SocialLink({ provider }) {
   return (
-    <a href={`http://localhost:1337/connect/${provider}`} className="link">
+    <a href={strapi.getProviderAuthenticationUrl(provider)} className="link">
       <Button type="button" social={provider} style={{ width: '100%' }}>
         <i className={`fab fa-${provider}`} style={{ marginRight: '10px' }} />
         {capitalize(provider)}
--- create-react-app-sdk/src/containers/App/index.js
+++ create-react-app-sdk/src/containers/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import AuthPage from '../../containers/AuthPage';
+import ConnectPage from '../../containers/ConnectPage';
 import HomePage from '../../containers/HomePage';
 import PrivateRoute from '../../containers/PrivateRoute';
 
@@ -16,9 +17,9 @@ class App extends Component {
             {/* A user can't go to the HomePage if is not authenticated */}
             <Route path="/auth/:authType/:id?" component={AuthPage} />
             <PrivateRoute path="/" component={HomePage} exact />
+            <Route exact path="/connect/:provider" component={ConnectPage} />
             {/* <PrivateRoute exact path="/product" component={ProductPage} />
             <PrivateRoute path="/:contentType/:id" component={EditPage} />
-            <Route exact path="/connect/:provider" component={ConnectPage} />
             <Route path="" component={NotFoundPage} /> */} */}
           </Switch>
         </div>
