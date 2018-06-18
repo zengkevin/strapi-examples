0fd6894a0a57de2803b1c6b15306f06614bf57dd
soupette <cyril.lpz@gmail.com>
2018-06-18 12:48:51 +0200

    Add product page

--- create-react-app-sdk/src/containers/ProductsPage/index.js
+++ create-react-app-sdk/src/containers/ProductsPage/index.js
@@ -4,7 +4,9 @@
  */
 
 import React from 'react';
+import { Link }  from 'react-router-dom';
 import strapi from '../../utils/Strapi';
+import Table from '../../components/Table';
 
 import './styles.scss';
 
@@ -20,10 +22,14 @@ class ProductsPage extends React.PureComponent {
 			const products = await strapi.getEntries('product');
 			this.setState({ products });
 		} catch(err) {
-			console.log(err);
+			// TODO handle error status 403 or 401
 			this.setState({ error: true });
 		}
 	}
+
+	onClick = (id) => {
+    this.props.history.push(`/product/${id}`);
+  }
 
 	render() {
 		const { error, products } = this.state;
@@ -31,11 +37,25 @@ class ProductsPage extends React.PureComponent {
 		if (error) {
 			return <div>An error occured</div>;
 		}
-		
+
 		return (
 			<div className="productPage">
-				<h1>See all products</h1>
 				<div className="container">
+					<div className="row">
+						<div className="col-md-4">
+							<h1>Products</h1>
+						</div>
+						<div className="col-md-4 offset-md-4 productPageLink">
+							<Link to="/product/create">Create a product</Link>
+						</div>
+					</div>
+					<div className="row">
+						<Table
+						data={this.state.products}
+						headers={['id', 'name', 'pictures', '']}
+						onClick={this.onClick}
+						/>
+					</div>
 				</div>
 			</div>
 		);
